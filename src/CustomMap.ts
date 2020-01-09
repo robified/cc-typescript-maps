412b48b90f7abae1bdca15700559a7eb84cc2e94
Robin <daorob@gmail.com>
2020-01-09 09:23:50 -0600

    Add user marker method. Add 'adding markers' notes.

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
     private googleMap: google.maps.Map;
 
@@ -10,4 +13,17 @@ export class CustomMap {
             }
         });
     }
+
+    // this is the bad code way
+    addUserMarker(user: User): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: user.location.lat,
+                lng: user.location.lng
+            }
+        });
+    }
+
+    addCompanyMarker(company: Company): void {}
 }
